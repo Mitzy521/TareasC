1a993a3c2e357ec92196226b034ec159f1f1f700
Mitzy521 <159392464+Mitzy521@users.noreply.github.com>
2025-08-09 08:31:00 -0600

    SolicitarNombre

---
+++ nombreEstudiante/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce tu nombre: ");
+        string nombreEstudiante = Console.ReadLine();
+
+        Saludar(nombreEstudiante);
+    }
+    
+    static void Saludar(string nombre)
+    {
+        Console.WriteLine("Hola, " + nombre);
+    }
+}

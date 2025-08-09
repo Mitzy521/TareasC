d4779d15c56846b8df1937db07d86f40febfb762
Mitzy521 <159392464+Mitzy521@users.noreply.github.com>
2025-08-09 08:30:16 -0600

    OperacionesLogicas

---
+++ operaciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a = 18;
+        int b = 6;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+
+        int n = 6;
+        int o = 4;
+        int p = 8;
+        int q = n + o * p;
+        int r = (n + o) * p;
+        int s = (n + o) - 6 * o + (12 * 4) / 3 + 12;
+        Console.WriteLine(q);
+        Console.WriteLine(r);
+        Console.WriteLine(s);
+    }
+}
+
